945cb56f9caf1d47881542d84ea4c2933f274eaf
Thomsen <isabeau.thomsen@maxpool.de>
2023-11-24 10:13:23 +0100

    CI + Test

---
+++ tests/unit/FactoryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace unit;
+use Configuration;
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+
+    public function testConfigWasFound(): void
+    {
+        $this->assertInstanceOf(Configuration::class, new Configuration());
+    }
+}
