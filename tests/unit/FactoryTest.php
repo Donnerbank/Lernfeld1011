51f5a9087fe6b385b84771348d8bbeb53a57493d
Thomsen <isabeau.thomsen@maxpool.de>
2023-11-24 10:27:43 +0100

    CI + Test

--- tests/unit/FactoryTest.php
+++ tests/unit/FactoryTest.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace unit;
+use Configuration\Configuration;
 use PHPUnit\Framework\TestCase;
 
 class FactoryTest extends TestCase
 {
     public function testConfigWasFound(): void
     {
-        $this->assertInstanceOf(\Configuration::class, new \Configuration());
+        $this->assertInstanceOf(Configuration::class, new Configuration());
     }
 }
