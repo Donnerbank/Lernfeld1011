f5dadf0b08da3ae4c536d1b89177d89c71b4d9e7
Thomsen <isabeau.thomsen@maxpool.de>
2023-11-30 09:00:23 +0100

    config wieder zurück nach src

--- tests/unit/FactoryTest.php
+++ tests/unit/FactoryTest.php
@@ -5,6 +5,9 @@ namespace unit;
 use Lernfeld1011\Configuration\Configuration;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers \Lernfeld1011\infrastructure\Factory
+ */
 class FactoryTest extends TestCase
 {
     public function testConfigWasFound(): void
